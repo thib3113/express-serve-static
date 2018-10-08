ca387adfed5ad950594ae1bd9f2c9fa9521d2640
thibaut severac <thib3113@gmail.com>
2018-10-08 11:32:34 +0200

    Update runner.js

--- runner.js
+++ runner.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-let buildFolder = path.join(__dirname,'../build');
+let buildFolder = path.join(__dirname,'public');
 
 app.use(express.static(buildFolder));
 app.use(fallback('index.html', { root: buildFolder }));
