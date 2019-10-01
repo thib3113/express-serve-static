e1a8fdd69c246bfcd04981604392dac131a9f0e0
thibaut severac <thib3113@gmail.com>
2019-10-01 10:21:40 +0200

    add compression

--- runner.js
+++ runner.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const fallback = require('express-history-api-fallback');
 const path = require("path");
+const compression = require('compression');
 const app = express();
 const port = 3000;
 
 let buildFolder = path.join(__dirname,'public');
 
+app.use(compression());
 app.use(express.static(buildFolder));
 app.use(fallback('index.html', { root: buildFolder }));
 
